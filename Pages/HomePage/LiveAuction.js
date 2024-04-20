b9969b04361d27257c8589c560d719b2d4e437e9
myusername <127469454+Shubham-garg1234@users.noreply.github.com>
2024-04-20 13:00:29 +0530

    Added timer for bid

--- Pages/HomePage/LiveAuction.js
+++ Pages/HomePage/LiveAuction.js
@@ -6,9 +6,27 @@ export default function LiveAuction({ auction }) {
 
   const searchparams = new URLSearchParams(window.location.search);
   const token = searchparams.get("token");
+  const[timerclass,settimerclass]=useState('timer');
 
-  console.log(auction)
+  const[timer,settimer]=useState(20);
   const [biddingItem , setbiddingItem] = useState(null)
+
+  useEffect(()=>{
+    const clock=setTimeout(()=>{
+      if(timer!=0){
+        const time=timer-0.1;
+        settimer(time.toFixed(1));
+      }else if(timer==0){
+        settimer(20);
+      }
+      if(timer<=5){
+        settimerclass('red-timer')
+      }else{
+        settimerclass('timer');
+      }
+    },100)
+    return () => clearTimeout(clock);
+  },[timer])
 
   const fetchBiddingItem = () => {
     
@@ -41,27 +59,30 @@ export default function LiveAuction({ auction }) {
 
   return (
     <div>
-
-        <h1 className='heading'>{auction.name}</h1>
-        <h2 className='itemName'>{biddingItem ? biddingItem.name : ''}</h2>
         <div className='flex-container'>
           <div>
+            <h1 className='heading'>{auction.name}</h1>
+            <br/><br/><br/><br/>
             <p className='price'>Starting price:  {biddingItem ? biddingItem.starting_price : ''}</p>
             <p className='name'>Seller Name:  {biddingItem ? biddingItem.sellerName : ''}</p>
+            <br></br>
             <p className='price'>Current Bid:  {biddingItem && biddingItem.current_bid ? biddingItem.current_bid : 'No Bid Yet'}</p>
             <p className='name'>Bidder Name:  {biddingItem ? biddingItem.bidderName : ''}</p>
           </div>
+          <div id="intersection-line"></div>
           <div>
+          <h2 className='itemName'>{biddingItem ? biddingItem.name : ''}</h2>
             <div className='outer-image'>
               <img className='' src = {require('../../assets/images/th.jpeg')} />
+              <div className='button-container'>
+                <button className='bid-button'>Make a bid of 5000</button>
+              </div>
             </div>
           </div>
           <div>
+            <div className={timerclass}> {timer} </div>
             <p className='description'>{biddingItem ? biddingItem.description : ''}</p>
           </div>
-        </div>
-        <div className='button-container'>
-          <button className='bid-button'>Make a bid of 5000</button>
         </div>
 
     </div>
